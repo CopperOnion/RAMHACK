34bb9fc27d12497c551acd088fe3c00209bf479b
unknown <lroy1@hawk.iit.edu>
2020-09-26 18:36:00 -0500

    updated fire.js

---
+++ flyquest/src/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyCk0ozYWyLTYANSOnZCmogN-Teco3iYxQs",
+    authDomain: "idyllic-psyche-290715.firebaseapp.com",
+    databaseURL: "https://idyllic-psyche-290715.firebaseio.com",
+    projectId: "idyllic-psyche-290715",
+    storageBucket: "idyllic-psyche-290715.appspot.com",
+    messagingSenderId: "564713871285",
+    appId: "1:564713871285:web:cf3b95e34f688a17482bfc",
+    measurementId: "G-RV9C0SB94Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+

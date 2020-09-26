61ade04469263249c178a9da5ecd010f51559817
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-09-26 12:47:42 -0400

    Set up API call with places API

--- flyquest/src/App.js
+++ flyquest/src/App.js
@@ -3,24 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+require("dotenv").config();
+
+
 function App() {
   const [data, setData] = useState({ hits: [] });
 
-  useEffect(() => {
- 
-    axios.get('http://api.reimaginebanking.com/atms?key=77d04e4dddfe94c57249580f5463ce18')
-      .then(res => {
-          console.log(res.data)
-      })
-      .catch(err => {
-          console.log(err)
-      })
 
+  useEffect(() => {
+    const GCLOUD = process.env.GCLOUD_KEY
+    console.log(GCLOUD)
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"; 
+    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=Museum+in+New+york&key=AIzaSyA9AAUYT6sm1_2znZwljF2ddRQyFBKGx-M`;
+    fetch(proxyurl+url,{
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json',
+      }    
+    })
+    .then(res=> res.json())
+    .then(data => console.log(data.results))
+    
   },[]);
   
   return (
     <div className="App">
-      
+       Hello teammates !
     </div>
   );
 }
